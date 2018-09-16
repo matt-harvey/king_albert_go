e19ba0001f47dd4594c2d7672c0c0c8a819da902
Matthew Harvey <software@matthewharvey.net>
2018-09-16 10:04:59 +1000

    Scaffold read loop.

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode/utf8"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
+func getRune(prompt string) (c rune) {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf(prompt)
+	for scanner.Scan() {
+		input := scanner.Bytes()
+		if utf8.RuneCount(input) != 1 {
+			fmt.Println("Please enter a single character.")
+			fmt.Printf(prompt)
+			continue
+		}
+		c, _ = utf8.DecodeRune(input)
+		break
+	}
+	return c
+}
 
 func main() {
-	fmt.Println("FIXME")
+	for {
+		char := getRune("Enter a character: ")
+		fmt.Printf("You entered: %c\n", char)
+	}
 }
